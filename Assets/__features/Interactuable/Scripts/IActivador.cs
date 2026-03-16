cda4b0f23cd3be97cacdea9b325fd233c85bcf24
ALANGMupv <alanguevara36@gmail.com>
2026-03-16 13:55:42 +0100

    Revert "Merge branch 'develop'"
    This reverts commit 4765d2fecebea50f808290e1d8f35c32d1eed4ee, reversing
    changes made to 110e6dcf9555e5291064a86ed834496af88b1381.

--- Assets/__features/Interactuable/Scripts/IActivador.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-/// <summary>
-/// Expone la posición del objeto que puede activar interacciones.
-/// </summary>
-public interface IActivador
-{
-    Vector3 Position { get; }
-}
--- Assets/__features/Interactuable/Scripts/JugadorActivador.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-/// <summary>
-/// Expone la posición del jugador para el sistema de interacción.
-/// </summary>
-public class JugadorActivador : MonoBehaviour, IActivador
-{
-    public Vector3 Position => transform.position;
-}
