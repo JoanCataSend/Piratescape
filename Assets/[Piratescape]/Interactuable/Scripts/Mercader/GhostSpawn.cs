463689099e7f633a45110ce9d30545586ca88122
sepubi <160240637+sepubi@users.noreply.github.com>
2026-05-11 15:17:33 +0200

    Fantasma se mueve en 8

--- Assets/[Piratescape]/Interactuable/Scripts/Mercader/GhostSpawn.cs
+++ Assets/[Piratescape]/Interactuable/Scripts/Mercader/GhostSpawn.cs
@@ -2,19 +2,37 @@ using UnityEngine;
 
 public class GhostSpawn : MonoBehaviour
 {
+    [Header("Puntos de aparición")]
     [SerializeField] private Transform[] puntosAparicion;
 
+    [Header("Movimiento en forma de 8")]
+    [SerializeField] private float amplitudX = 0.8f;
+    [SerializeField] private float amplitudZ = 0.4f;
+    [SerializeField] private float velocidad = 1.2f;
+
     private int ultimoPunto = -1;
+    private Vector3 posicionBase;
+    private bool estaDeNoche;
 
     public void EmpezarNoche()
     {
         AparecerEnPuntoNocturno();
         gameObject.SetActive(true);
+        estaDeNoche = true;
     }
 
     public void EmpezarDia()
     {
+        estaDeNoche = false;
         gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!estaDeNoche)
+            return;
+
+        MoverEnFormaDeOcho();
     }
 
     private void AparecerEnPuntoNocturno()
@@ -34,8 +52,20 @@ public class GhostSpawn : MonoBehaviour
         while (nuevoPunto == ultimoPunto && puntosAparicion.Length > 1);
 
         ultimoPunto = nuevoPunto;
+
         transform.position = puntosAparicion[nuevoPunto].position;
+        posicionBase = transform.position;
 
         Debug.Log("Fantasma apareció en: " + puntosAparicion[nuevoPunto].name);
+    }
+
+    private void MoverEnFormaDeOcho()
+    {
+        float tiempo = Time.time * velocidad;
+
+        float x = Mathf.Sin(tiempo) * amplitudX;
+        float z = Mathf.Sin(tiempo * 2f) * amplitudZ;
+
+        transform.position = posicionBase + new Vector3(x, 0f, z);
     }
 }
